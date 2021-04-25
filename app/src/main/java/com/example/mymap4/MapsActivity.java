b8ffcb356e4fcbecf551ff4c86e967b91c9f846c
jea7982@naver.com <jea7982@naver.com>
2021-04-25 13:52:06 +0900

    지도 0425

--- app/src/main/java/com/example/mymap4/MapsActivity.java
+++ app/src/main/java/com/example/mymap4/MapsActivity.java
@@ -38,9 +38,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng Korea = new LatLng(36, 127);
+        mMap.addMarker(new MarkerOptions().position(Korea).title("Here"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Korea));
     }
 }
